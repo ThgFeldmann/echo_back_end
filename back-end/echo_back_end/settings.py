7e0b19aa621ba4f664af4071903a2a8268f7c827
Thiago <thiagofeldmann06@gmail.com>
2025-11-25 10:41:40 -0300

    debug to true

--- back-end/echo_back_end/settings.py
+++ back-end/echo_back_end/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-%ippe$+pra_a-&f6z=wqk6(*q@!%b#$nntx!6swr6#q5_cfk9c"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app', 'https://echonetwork-ten.vercel.app/']
 
