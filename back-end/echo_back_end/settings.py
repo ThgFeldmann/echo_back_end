b0a05d6e186d809656baa80218504a5b8c9018e5
Thiago <thiagofeldmann06@gmail.com>
2025-12-31 15:05:29 -0300

    update: altered to production db

--- back-end/echo_back_end/settings.py
+++ back-end/echo_back_end/settings.py
@@ -94,23 +94,23 @@ WSGI_APPLICATION = "echo_back_end.wsgi.app"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-#DATABASES = {
-        #'default': {
-        #    'ENGINE': 'django.db.backends.postgresql',
-        #    'NAME': os.getenv('PGDATABASE'),
-        #    'USER': os.getenv('PGUSER'),
-        #    'PASSWORD': os.getenv('PGPASSWORD'),
-        #    'HOST': os.getenv('PGHOST'),
-        #    'PORT': os.getenv('PGPORT'),
-        #}
-    #}
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('PGDATABASE'),
+            'USER': os.getenv('PGUSER'),
+            'PASSWORD': os.getenv('PGPASSWORD'),
+            'HOST': os.getenv('PGHOST'),
+            'PORT': os.getenv('PGPORT'),
+        }
     }
-}
+
+#DATABASES = {
+    #"default": {
+    #    "ENGINE": "django.db.backends.sqlite3",
+    #    "NAME": BASE_DIR / "db.sqlite3",
+    #}
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
