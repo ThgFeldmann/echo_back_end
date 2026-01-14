5bddc051651f8e1b40355e420a99c533f3a672c4
Thiago <thiagofeldmann06@gmail.com>
2026-01-14 14:10:00 -0300

    test: print pgPassword

--- back-end/echo_back_end/settings.py
+++ back-end/echo_back_end/settings.py
@@ -10,9 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
+print(f"DEBUG_VALUE: {os.getenv('PGPASSWORD')}")
 from pathlib import Path
-
-from django.conf.global_settings import DATABASES, MEDIA_ROOT
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
